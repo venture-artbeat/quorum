bafaf6d9e4f497bb624db2bf230971bab4926f2f
Ara Adkins <iamrecursion@users.noreply.github.com>
2024-01-16 16:32:04 -0700

    Implement basic precompiles for BW6-761 (#5)
    This PR includes working precompiles for:
    
    - G1 point addition
    - G2 point addition
    - G1 scalar multiplication
    - G2 scalar multiplication
    - BW6-761 pairing checks
    
    Note that the test coverage is very low at the moment, and only tests
    the happy paths. This is in aid of making things available before the
    break to avoid the potential for any blockers. More tests are to come.

--- common/ecprecompile.go
+++ common/ecprecompile.go
@@ -1,5 +1,26 @@
+// Package common
+//
+// We need these as constants, but in a type that doesn't support easy constant initialization, so we have them here
+// as functions.
+
 package common
 
-func ECPrecompileHelloContractAddress() Address {
-	return BytesToAddress([]byte{byte(0x8a)})
+func ECPrecompileBW6761G1AddContractAddress() Address {
+	return BytesToAddress([]byte{byte(0x80)})
+}
+
+func ECPrecompileBW6761G1ScalarMulContractAddress() Address {
+	return BytesToAddress([]byte{byte(0x81)})
+}
+
+func ECPrecompileBW6761G2AddContractAddress() Address {
+	return BytesToAddress([]byte{byte(0x82)})
+}
+
+func ECPrecompileBW6761G2ScalarMulContractAddress() Address {
+	return BytesToAddress([]byte{byte(0x83)})
+}
+
+func ECPrecompileBW6761PairingCheckContractAddress() Address {
+	return BytesToAddress([]byte{byte(0x84)})
 }
